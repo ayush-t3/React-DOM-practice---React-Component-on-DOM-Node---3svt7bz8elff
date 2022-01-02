58b4e75eae1f05c1b9ed7f5e5cbfa2db0ff2ec61
Ayush Tiwari <59754907+ayush-t3@users.noreply.github.com>
2022-01-03 00:28:36 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 export default function App(){
   return(
-    <>
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    </>
+    <div>
+
+    ReactDOM.render(<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>, document.getElementById("root"))
+    </div>
     
 );
 }
